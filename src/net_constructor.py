7a2f4e060401a2b1f0b0b885564b2d070db989ae
ziyu24 <40757215+ziyu24@users.noreply.github.com>
2024-11-14 00:51:43 +0800

    1.以手写数字识别项目为依托，构建了模型训练、推理、可视化等操作 2.模型训练可以达到较高精度，模型推理能力很差

---
+++ src/net_constructor.py
@@ -0,0 +1,24 @@
+"""
+Created on 11 13, 2024
+@author: <Cui>
+@bref: 实现 LeNet 类
+"""
+
+import torch
+
+
+class LeNet(torch.nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.fc1 = torch.nn.Linear(28 * 28, 64)
+        self.fc2 = torch.nn.Linear(64, 64)
+        self.fc3 = torch.nn.Linear(64, 64)
+        self.fc4 = torch.nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = torch.nn.functional.relu(self.fc1(x))
+        x = torch.nn.functional.relu(self.fc2(x))
+        x = torch.nn.functional.relu(self.fc3(x))
+        x = torch.nn.functional.log_softmax(self.fc4(x), dim=1)
+        return x
